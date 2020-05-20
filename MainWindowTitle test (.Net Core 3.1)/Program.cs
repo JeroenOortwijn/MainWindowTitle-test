3fd16c4b8ebe9f871ac5674e544846062eebef92
Jeroen Oortwijn <51476918+JeroenOortwijn@users.noreply.github.com>
2020-05-20 18:32:17 +0200

    Add project files.

---
+++ MainWindowTitle test (.Net Core 3.1)/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MainWindowTitle_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("MainWindowTitle test (.Net Core 3.1)");
+            System.Console.WriteLine("Press ESC to stop.");
+
+            string oldMainWindowTitle = null;
+            var process = System.Diagnostics.Process.Start("notepad");
+            try
+            {
+                do
+                {
+                    process?.Refresh();
+                    string mainWindowTitle = process.MainWindowTitle;
+                    if (mainWindowTitle != oldMainWindowTitle)
+                    {
+                        oldMainWindowTitle = mainWindowTitle;
+                        System.Console.WriteLine(mainWindowTitle);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                } while (!(System.Console.KeyAvailable && System.Console.ReadKey(true).Key == System.ConsoleKey.Escape));
+            }
+            finally
+            {
+                process?.Kill();
+            }
+        }
+    }
+}
